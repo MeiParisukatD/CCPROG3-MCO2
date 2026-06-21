e59bbbbec0d6d8479a1edfb19e169012870b16cf
Kara Porciuncula <166294744+k-porky@users.noreply.github.com>
2026-06-22 02:11:54 +0800

    Dungeon and character class/subclass skeletons

---
+++ Game.java
@@ -0,0 +1,13 @@
+//Playable file
+import Character_classes.*;
+import Dungeon_classes.*;
+
+public class Game {
+    public initializeGame() {
+        //TODO
+    }
+
+    public static void main(String[] args) {
+        //TODO
+    }
+}
